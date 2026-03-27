2d5f7c0974859c5d571e424d678fa285fabb0acc
Leon Chen <cxl_forever@163.com>
2026-03-27 14:56:04 +1100

    fix file name casing and import paths

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Video from './video.jsx'
-import PlaceholderPage from './PlaceholderPage.jsx'
-import About from './About.jsx'
-import Support from './Support.jsx'
-import GetInvolved from './GetInvolved.jsx'
-import Essence from './Essence.jsx'
+import PlaceholderPage from './placeholderPage.jsx'
+import About from './about.jsx'
+import Support from './support.jsx'
+import GetInvolved from './getInvolved.jsx'
+import Essence from './essence.jsx'
 
 
 
@@ -21,8 +21,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/involved" element={<GetInvolved />} />
-        <Route path="/events" element={<PlaceholderPage title="Events" />} />
-        <Route path="/news" element={<PlaceholderPage title="News" />} />
+        <Route path="/placeholderPage" element={<PlaceholderPage title="Placeholder Page" />} />
         <Route path="/video" element={<Video title="Video" />} />
         <Route path="/about" element={<About title="About CACHI" />} />
         <Route path="/support" element={<Support title="Support & services" />} />
